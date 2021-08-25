0ecaa3ed745f308cf66754cd20c99d38ee8e3cd0
shandamckinney <mckinney.shanda@gmail.com>
2021-08-24 19:18:25 -0500

    putting together home about and projects

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    fadeInOnEnterAnimation({anchor: 'enter'})
+  ]
 })
 export class HomeComponent implements OnInit {
 
