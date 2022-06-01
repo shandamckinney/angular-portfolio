11dca7bcfb7c707b3872e91f1250fc79f4ba85c1
Shanda McKinney <mckinney.shanda@gmail.com>
2022-05-31 23:23:44 -0500

    changed whole portfolio

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,20 +1,46 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-import { fadeInOnEnterAnimation, fadeOutOnLeaveAnimation } from 'angular-animations';
-
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   animations: [
-    fadeInOnEnterAnimation({anchor: 'enter'})
+    trigger('slideRight', [
+      transition(':enter', [
+        style({transform: 'translateX(-20%)', opacity: .5}),
+        animate('1000ms ease-in', style({transform: 'translateX(0%)'}))
+      ]),
+    ]),
+    trigger('slideLeft', [
+      transition(':enter', [
+        style({transform: 'translateX(20%)', opacity: .5}),
+        animate('1000ms ease-in', style({transform: 'translateX(0%)', opacity: 100}))
+      ]),
+    ]),
+    trigger('slideUp', [
+      transition(':enter', [
+        style({transform: 'translateY(40%)', opacity: .2}),
+        animate('1000ms ease-in', style({transform: 'translateY(0%)', opacity: 100}))
+      ]),
+    ])
   ]
+
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
+  }
+
+  addClass(event: any): void {
+    event.target.className += ' active';
+  }
+
+  removeClass(event: any): void {
+    event.target.className = event.target.className.replace(' active', '');
   }
 
 }
